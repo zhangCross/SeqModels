4eb5cc0365085546cf9471cb451cc62737cf80bd
zc <cement1@126.com>
2022-07-25 18:34:26 +0800

    backward prop

--- generate_university_name/rnn.py
+++ generate_university_name/rnn.py
@@ -80,11 +80,11 @@ def rnn_step_backward(dy, gradients, parameters, xt_oh, a, a_prev):
     gradients['dWya'] += np.dot(dy, a.T)
     gradients['dby'] += dy
     da = np.dot(parameters['Wya'].T, dy) + gradients['da_next']  # backprop into h
-    daraw = (1 - a * a) * da  # backprop through tanh nonlinearity
-    gradients['db'] += daraw
-    gradients['dWax'] += np.dot(daraw, xt_oh.T)
-    gradients['dWaa'] += np.dot(daraw, a_prev.T)
-    gradients['da_next'] = np.dot(parameters['Waa'].T, daraw)
+    dtanh = (1 - a * a) * da  # backprop through tanh
+    gradients['db'] += dtanh
+    gradients['dWax'] += np.dot(dtanh, xt_oh.T)
+    gradients['dWaa'] += np.dot(dtanh, a_prev.T)
+    gradients['da_next'] = np.dot(parameters['Waa'].T, dtanh)
     return gradients
 
 
